e2c90df16afaeb671221f063f0255e8f879637d4
José Braga <jgarciabraga@gmail.com>
2025-04-01 23:11:16 -0300

    finished generate data function

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 import data_starter.extract as extract
 import data_starter.load as load
 import data_starter.transform as transform
+import data_starter.generate_data as generate_data
 
 if __name__ == '__main__':
-    data_frame_list = extract.extract_from_excel('data/input')
+    path_dir_input = "data/input"
+    generate_data.generate_data(50, path_dir_input)
+    data_frame_list = extract.extract_from_excel(path_dir_input)
     data_frame = transform.concat_dataframe_list(data_frame_list)
     print(load.load_data(data_frame, 'output.xlsx', 'data/output'))
