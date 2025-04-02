2ed026d5ee1d0ec24cb40de3a565bfd2f4363b40
José Braga <jgarciabraga@gmail.com>
2025-04-01 21:29:56 -0300

    add test of transform

--- tests/unit_test.py
+++ tests/unit_test.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import pytest
 import src.data_starter.extract as extract
+import src.data_starter.transform as transform
 
 @pytest.fixture
 def mock_inputfolder(tmp_path):
@@ -15,6 +16,16 @@ def mock_inputfolder(tmp_path):
     df_2.to_excel(file_2, index=False)
 
     return tmp_path, [df_1, df_2]
+
+@pytest.fixture
+def mock_cocatenate_data():
+
+    df_1 = pd.DataFrame({"a": [1, 2], "b": [3, 4]})
+    df_2 = pd.DataFrame({"a": [5, 6], "b": [7, 8]})
+    list_dataframes = [df_1, df_2]
+    df_final = pd.concat(list_dataframes, ignore_index=True)
+
+    return list_dataframes, df_final
 
 def test_extract(mock_inputfolder):
     test_path, test_dataframe_list = mock_inputfolder
@@ -38,4 +49,13 @@ def test_extract(mock_inputfolder):
 
     for df_res, df_test in zip(dataframe_list, test_dataframe_list):
         pd.testing.assert_frame_equal(df_res, df_test)
+
+def test_transform(mock_cocatenate_data):
+    test_list_dataframes, test_df_final = mock_cocatenate_data
+    res_df_final = transform.concat_dataframe_list(test_list_dataframes)
+
+    assert len(res_df_final) == len(test_df_final)
+
+    pd.testing.assert_frame_equal(res_df_final, test_df_final)
+
 
